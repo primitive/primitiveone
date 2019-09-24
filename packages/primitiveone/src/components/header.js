52847fe663c507d2834ddc20e9a2c8b3eb74ca39
Shaun Knowles <shaun@primitivedigital.uk>
2019-09-24 09:28:41 +0100

    chore: Update package.json

--- packages/primitiveone/src/components/header.js
+++ packages/primitiveone/src/components/header.js
@@ -1,25 +1,64 @@
 import React from "react";
 import { connect, styled } from "frontity";
-import { Container, Row, Col, Tabs, Tab} from "react-bootstrap";
+import { Container, Row, Col, Tabs, Tab } from "react-bootstrap";
 import Link from "./link";
 
 const Header = ({ state }) => (
-    <Ele>
-      <Container>
+  <Ele>
+    <Container>
       <StyledLink link="/web-design/">
         <Title>{state.frontity.title}</Title>
-        <p className="subslab"><span className="slab">We Make </span>Fast and Efficient</p>
+        <Intro className="subslab"><span className="slab">We Make </span>Fast and Efficient</Intro>
         <Description>{state.frontity.description}</Description>
       </StyledLink>
-      </Container>
-    </Ele>
+    </Container>
+  </Ele>
 );
 
 export default connect(Header);
 
-const Ele = styled.header``;
-const Title = styled.h2``;
-const Description = styled.h4``;
+const Ele = styled.header`
+  padding: 0 1rem;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+
+  a {
+    text-decoration: none;
+    padding: 15px;
+    display: block;
+}
+`;
+const Title = styled.h2`
+  color: #fff;
+  font-size: 5rem;
+  letter-spacing: 1px;
+  text-shadow: 1px 1px 2px #477C16;
+  margin-bottom: .5rem;
+`;
+const Intro = styled.p`
+  color: #fff;
+  font-size: 1.8rem;
+  font-style: italic;
+  letter-spacing: 1px;
+  text-shadow: 1px 1px 2px #477C16;
+  margin-bottom: .5rem;
+
+  span {
+    color: #fff;
+    font-size: 1.5rem;
+    font-style: normal;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+ }
+`;
+const Description = styled.h4`
+  color: #fff;
+  font-size: 1.5rem;
+  letter-spacing: 1px;
+  text-shadow: 1px 1px 2px #477C16;
+`;
 const StyledLink = styled(Link)`
   text-decoration: none;
 `;
