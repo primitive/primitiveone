6f1a2451c9a4f60808ce6589b21c486859d337db
Shaun Knowles <sknow@skmbp1.home>
2020-02-29 19:02:23 +0000

    add dust!

---
+++ packages/primitiveone/src/components/dust/animation.js
@@ -0,0 +1,173 @@
+import { keyframes } from "frontity";
+
+const colour = {
+  bounce: "blue"
+}
+
+const jump = keyframes`
+  from, 20%, 53%, 80%, to {
+    transform: translate3d(0,0,0);
+  }
+  40%, 43% {
+    transform: translate3d(0, -30px, 0);
+  }
+  70% {
+    transform: translate3d(0, -15px, 0);
+  }
+  90% {
+    transform: translate3d(0,-4px,0);
+  }
+`;
+
+const heartbeat = keyframes`
+    0% {
+      transform: scale( .75);
+    }
+    20% {
+      transform: scale( 1);
+    }
+    40% {
+      transform: scale( .75);
+    }
+    60% {
+      transform: scale( 1);
+    }
+    80% {
+      transform: scale( .75);
+    }
+    100% {
+      transform: scale( .75);
+    }
+`;
+
+// animation: pulse 1.5s infinite;
+const sonar = keyframes`
+  0% {
+    @include transform(scale(.9));
+  }
+  70% {
+    @include transform(scale(1));
+    box-shadow: 0 0 0 50px rgba(#5a99d4, 0);
+  }
+  100% {
+    @include transform(scale(.9));
+    box-shadow: 0 0 0 0 rgba(#5a99d4, 0);
+  }
+`;
+
+// animation: flash linear 1s infinite;
+const flash = keyframes`
+	0% { opacity: 1; } 
+	50% { opacity: .1; } 
+	100% { opacity: 1; }
+`;
+
+const pop = keyframes`
+  0% {
+    box-shadow: 0 0 0 4px ${colour.bounce};
+    opacity: 1;
+    transform: scale(1);
+  }
+  25% {
+    box-shadow: 0 0 0 1px ${colour.bounce};
+    opacity: 1;
+    transform: scale(0.97);
+  }
+  50% {
+    box-shadow: 0 0 0 7px ${colour.bounce};
+    opacity: 1;
+    transform: scale(1.01);
+  }
+  75% {
+    box-shadow: 0 0 0 4px ${colour.bounce};
+    opacity: 1;
+    transform: scale(1);
+  }
+  100% {
+    box-shadow: 0 0 0 5px ${colour.bounce};
+    opacity: 1;
+  }
+`;
+
+const animatedgradient = keyframes`
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+`;
+
+// animation: underlinewoosh 6s linear infinite;
+const underlinewoosh = keyframes`
+  0% {
+    transform: translate3d(0%, 0%, 0);
+  }
+  100% {
+    transform: translate3d(-75%, 0%, 0);
+  }
+`;
+
+
+
+
+// DataMap 
+
+const africa = keyframes`
+  0% {
+    background-color: #eaf803;
+  }
+  100% {
+    background-color: #222;
+  }
+`;
+
+const asia = keyframes`
+  0% {
+    background-color: orange;
+  }
+  100% {
+    background-color: #222;
+  }
+`;
+
+const australia = keyframes`
+  0% {
+    background-color: rgb(122, 95, 255);
+  }
+  100% {
+    background-color: #222;
+  }
+`;
+
+const europe = keyframes`
+  0% {
+    background-color: pink;
+  }
+  100% {
+    background-color: #222;
+  }
+`;
+
+const northamerica = keyframes`
+  0% {
+    background-color: #87af4e;
+  }
+  100% {
+    background-color: #222;
+  }
+`;
+
+const southamerica = keyframes`
+  0% {
+    background-color: rgb(1, 255, 137);
+  }
+  100% {
+    background-color: #222;
+  }
+`;
+
+export {jump, heartbeat, sonar, flash, pop, animatedgradient, underlinewoosh, africa, asia, australia, europe, northamerica, southamerica };
---
+++ packages/primitiveone/src/components/dust/font-faces.js
@@ -0,0 +1,71 @@
+  
+import React from "react";
+import { Global, css, connect } from "frontity";
+
+/*
+import InterMedium from "../../fonts/inter/Inter-Medium.woff2";
+import InterBold from "../../fonts/inter/Inter-Bold.woff2";
+import InterSemiBold from "../../fonts/inter/Inter-SemiBold.woff2";
+import InterMediumUS from "../../fonts/inter/Inter-Medium-US-ASCII.woff2";
+import InterBoldUS from "../../fonts/inter/Inter-Bold-US-ASCII.woff2";
+import InterSemiBoldUS from "../../fonts/inter/Inter-SemiBold-US-ASCII.woff2";
+import InterMediumLatin from "../../fonts/inter/Inter-Medium-LATIN.woff2";
+import InterBoldLatin from "../../fonts/inter/Inter-Bold-LATIN.woff2";
+import InterSemiBoldLatin from "../../fonts/inter/Inter-SemiBold-LATIN.woff2";
+*/
+
+// sk-dev: WEBFONTS REFINE/STANDARD
+// https://community.frontity.org/t/frontity-release-4-november-2019/795
+// test performance impact of font loading: NOT working, cannot remove cdn url???
+// Import your own font file.
+import heptaSlab from "../fonts/hepta-slab-v6-latin-500.woff";
+
+const FontFace = ({ state }) => {
+  let fonts = null;
+  let fontDisplay = "swap";
+  switch (state.theme.fontSets) {
+    case "us-ascii":
+      fonts = [ /*InterMediumUS, InterSemiBoldUS, InterBoldUS */ heptaSlab ];
+      fontDisplay = "block";
+      break;
+    case "latin":
+      fonts = [ /* InterMediumLatin, InterSemiBoldLatin, InterBoldLatin */ heptaSlab];
+      break;
+    default:
+      fonts = [ /* InterMedium, InterSemiBold, InterBold */ heptaSlab];
+  }
+
+
+  /* hepta-slab-500 - latin */
+  /* see: http://google-webfonts-helper.herokuapp.com/fonts/hepta-slab?subsets=latin */
+
+  return (
+    <Global
+      styles={css`
+        @font-face {
+          font-family: "Hepta Slab";
+          font-style: normal;
+          font-weight: 500;
+          src: url(${fonts[0]}) format("woff2");
+          font-display: ${fontDisplay};
+        }
+        @font-face {
+          font-family: "Inter";
+          font-style: normal;
+          font-weight: 600;
+          src: url(${fonts[1]}) format("woff2");
+          font-display: ${fontDisplay};
+        }
+        @font-face {
+          font-family: "Inter";
+          font-style: normal;
+          font-weight: 700;
+          src: url(${fonts[2]}) format("woff2");
+          font-display: ${fontDisplay};
+        }
+      `}
+    />
+  );
+};
+
+export default connect(FontFace);
