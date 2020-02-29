9bd9064458e8fa75ff64df1be1cf8128f334f6c9
Shaun Knowles <sknow@skmbp1.home>
2020-02-29 18:58:51 +0000

    add mountains (aka. beautiful views)

---
+++ packages/primitiveone/src/components/mountains/index.js
@@ -0,0 +1,186 @@
+import React from "react";
+import { connect, Global, Head, styled, Slot } from "frontity";
+
+import ReactGA from 'react-ga';
+
+//import MetaTitle from "../pebbles/meta-title";
+import Loading from "../pebbles/loading";
+
+import Header from "../rocks/header";
+import Nav from "../rocks/nav";
+import Footer from "../rocks/footer";
+import Posts from "../rocks/postlist";
+import GlobalPromo from "../rocks/global-promo";
+import GlobalSocial from "../rocks/social/social-section";
+
+import Home from './page-home';
+import Post from "./post";
+//import Page from "./page";
+import Page404 from "./page404";
+//import PageError from "./page-error";
+
+import Timelines from "../timeline/ct-timelines";
+import Timeline from "../timeline";
+import SubTimeline from "../timeline/cpt-subtimeline";
+
+
+// sk-dev: cannot use local bs css??? all attempts to replace cdn breaks layout???
+// Do I really need all this???
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import bsCSS from 'bootstrap/dist/css/bootstrap.css';
+// console.log(bsCSS);
+
+import globalStyles from '../dust/style.css';
+import FontFaces from "../dust/font-faces";
+
+
+const Theme = ({ state }) => {
+
+  const data = state.source.get(state.router.link);
+  const display = state.theme.config;
+
+  //console.log("@index: data", data);
+  //console.log("@index: display", display);
+
+  // sk-dev: basic tracking snippet to be replaced with frontity package once released
+  function initializeReactGA() {
+    ReactGA.initialize('UA-61815763-1');
+    ReactGA.pageview(state.router.link);
+  }
+  initializeReactGA();
+
+  return (
+    <>
+      {
+        /*
+          Add global styles for the whole site, like body or a's or font-faces. 
+          Not classes here because we use CSS-in-JS. Only global HTML tags.
+        */
+      }
+      <Global styles={[globalStyles]} />
+
+      {/* sk-dev: To TEST <FontFaces /> */}
+
+      {/* sk-dev: To TEST Add some metatags to the <head> of the HTML. 
+      <MetaTitle />*/}
+
+      <Head>
+
+        <title>{state.frontity.title} - Primitive Digital</title>
+
+        <meta name="description" content={state.frontity.description} />
+        <html lang="en" />
+
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous"
+        />
+
+        { /* <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Amatic+SC|Comfortaa|Pacifico|Playfair+Display:700|Slabo+27px&display=swap" rel="stylesheet" />  */}
+        <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Amatic+SC|Comfortaa|Hepta+Slab|Pacifico|Playfair+Display:700|Slabo+27px&display=swap" rel="stylesheet" />
+
+        <link rel="canonical" href="https://primitivedigital.co.uk" />
+
+        <link rel="dns-prefetch" SameSite="None" Secure href="https://www.google-analytics.com" />
+        <link rel="dns-prefetch" SameSite="None" Secure href="https://stats.g.doubleclick.net" />
+
+      </Head>
+
+      <HeadWrapper className="wrap-header">
+        <Header />
+        <Nav />
+      </HeadWrapper>
+
+      <BodyWrapper className="wrap-body">
+
+
+          {(data.isFetching && <Loading />) ||
+            (data.isHome && <Home />) ||
+            (data.isPostArchive && <Posts />) ||
+            (data.isPostType && <Post />) ||
+            (data.is404 && <Page404 />) ||
+            (data.isError && <PageError />) ||
+            (data.isTemporalEventsArchive && <Timeline />) ||
+            (data.isTimelineType && <SubTimeline />) ||
+            (data.isTimelines && <Timelines />)}
+
+
+      </BodyWrapper>
+
+      {display.global.showPromo && <GlobalPromo />}
+
+      {display.global.showSocial && <GlobalSocial />}
+
+      { /* ??? global sections promo + social stuff - implement conditional slots/fills ??? data.isHome && <Promo /> */}
+      { /* display.showPromo && <Slot name="slot-1" /> */}
+
+      <Footer />
+    </>
+  );
+};
+
+export default connect(Theme);
+
+const HeadWrapper = styled.div`
+
+  display: block;
+  background-color: #87af4e;
+  background: url(https://res.cloudinary.com/primitivedigital/image/upload/f_auto/v1569316857/header/website_header_n1jxds.jpg) #87af4e;
+  background-position-x: 10%;
+  background-position-x: top;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  .container {
+    background: none;
+  }
+
+  @media screen and (min-width: 576px) {
+    background: none;
+    background-color: #87af4e;
+
+    header a { margin-left: 1rem; }
+
+    .container { background-position-x: 0; }
+    
+    .mast {
+      background: url(https://res.cloudinary.com/primitivedigital/image/upload/f_auto/v1569316857/header/website_header_n1jxds.jpg) #87af4e;
+      background-position-x: 10%;
+      background-position-y: top;
+      background-repeat: no-repeat;
+      background-size: auto;
+    }
+  }
+
+@media screen and (min-width: 768px) {
+  header a { margin-left: 20%; }
+  h4 { font-size: 2rem; }
+  .container { background-position-x: 5%; }
+}
+
+@media screen and (min-width: 992px) {
+  header a { margin-left: 30%; }
+  .container { background-position-x: 10%; }
+}
+
+@media screen and (min-width: 1200px) {
+  header a { margin-left: 35%; }
+  .container { background-position-x: 20%; }
+}
+
+`;
+
+const BodyWrapper = styled.div`
+  margin:0;
+  padding: 0;
+  display: block;
+
+  background-image: linear-gradient(
+    180deg,
+    rgba(66, 174, 228, 0.1),
+    rgba(66, 174, 228, 0)
+  );
+
+`;
---
+++ packages/primitiveone/src/components/mountains/post.js
@@ -0,0 +1,175 @@
+import React, { useEffect } from "react";
+import { connect, styled } from "frontity";
+import { Container, Row, Col } from "react-bootstrap";
+
+import List from "../rocks/postlist";
+import Link from "../pebbles/link";
+import FeaturedMedia from "../pebbles/featured-media";
+
+const Post = ({ state, actions, libraries }) => {
+
+  // Get info of current post.
+  const data = state.source.get(state.router.link);
+  // Get the the post.
+  const post = state.source[data.type][data.id];
+  // Get the author.
+  const author = state.source.author[post.author];
+  // Get a date for humans.
+  const date = new Date(post.date);
+
+  const postType = data.type;
+  const postId = data.id;
+
+  // Prefetch home posts and the list component.
+  useEffect(() => {
+    actions.source.fetch("/");
+    List.preload();
+  }, []);
+
+  /*
+  let titleTag = ['h1','h2','h3'];
+  let titleTagClose = ['</h1>','</h2>','</h3>'];
+  console.log(state.source);
+  */
+
+  return data.isReady ? (
+    <StyledMain className={"post " + postType} id={postType + "-" + postId}>
+
+      <Container>
+
+        <PostHead className="row">
+          <Col>
+            <PostTitle dangerouslySetInnerHTML={{ __html: post.title.rendered }} />
+          </Col>
+        </PostHead>
+
+        {data.isPost && (
+          <Row className="postMeta">
+            <Col>
+              <StyledLink link={author.link}>
+                <PostAuthor> By <b>{author.name}</b></PostAuthor>
+              </StyledLink>
+
+              <PostDate> {" "} on <b>{date.toDateString()}</b></PostDate>
+            </Col>
+          </Row>
+        )}
+
+        <Row>
+          <Col>
+
+            {state.theme.featured.showOnPost && (<FeaturedMedia id={post.featured_media} />)}
+
+          </Col>
+        </Row>
+
+        <Row>
+          <PostBody>
+            <libraries.html2react.Component html={post.content.rendered} />
+          </PostBody>
+        </Row>
+
+      </Container>
+      
+    </StyledMain>
+  ) : null;
+};
+
+export default connect(Post);
+
+const StyledMain = styled.main`
+  padding: 5px;
+`;
+
+const StyledLink = styled(Link)`
+  padding: 15px 0;
+`;
+
+const PostHead = styled.header`
+
+`;
+
+const PostTitle = styled.h1`
+  color: rgba(12, 17, 43);
+`;
+
+const PostAuthor = styled.p`
+  color: rgba(12, 17, 43, 0.9);
+  font-size: 0.9em;
+  display: inline;
+`;
+
+const PostDate = styled.p`
+  color: rgba(12, 17, 43, 0.9);
+  font-size: 0.9em;
+  display: inline;
+`;
+
+const PostBody = styled(Col)`
+  color: rgba(12, 17, 43, 0.8);
+  word-break: break-word;
+  padding-top: 2rem;
+
+  p {
+    line-height: 1.6em;
+  }
+
+  a {
+    color: rgb(31, 56, 197);
+    text-decoration: underline;
+  }
+
+  img {
+    width: 100%;
+    object-fit: cover;
+    object-position: center;
+  }
+
+  figure {
+    margin: 24px auto;
+    /* next line overrides an inline style of the figure element. 
+    width: 100% !important;
+    */
+    figcaption {
+      font-size: 0.7em;
+    }
+  }
+
+  iframe {
+    display: block;
+    margin: auto;
+  }
+
+
+
+
+
+
+
+
+  /* WordPress Core Align Classes */
+
+  @media (min-width: 420px) {
+    img.aligncenter,
+    img.alignleft,
+    img.alignright {
+      width: auto;
+    }
+
+    .aligncenter {
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+    }
+
+    .alignright {
+      float: right;
+      margin-left: 24px;
+    }
+
+    .alignleft {
+      float: left;
+      margin-right: 24px;
+    }
+  }
+`;
