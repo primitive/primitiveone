4528c567d09a096460f251f492d6573f03d8fb44
Shaun Knowles <sknow@skmbp1.home>
2020-03-25 21:40:34 +0000

    wip

--- packages/primitiveone/src/components/scenes/discoball.js
+++ packages/primitiveone/src/components/scenes/discoball.js
@@ -1,27 +1,199 @@
-// based on codepen by Marty
-// https://codepen.io/okra/pen/fDuzC
-
-
 import React from 'react';
 import { styled, css } from "frontity";
-import { africa, asia, australia, europe, northamerica, southamerica } from "./glamourmagic";
+import { rotateDiscoBall, rotateDiscoBallMiddle, reflect } from "./glamourmagic";
+//import "../sand/discoball";
 
-const DataMap = () => {
+// based on original codepen by Marty
+// https://codepen.io/okra/pen/fDuzC
 
-  const noOfCells = 1925; // 35x55 grid
-  const grid = [...Array(noOfCells)].map((e, i) => <div className="cell" key={i}></div>)
+const JungleDiscoBall = () => {
+
+  // do math
+  const radius = 50;
+  const squareSize = 10;
+  const prec = 19.55;
+  const fuzzy = 0.001;
+  const inc = (Math.PI - fuzzy) / prec;
+
+  let discoBall = <></>;
+  let discoTiles = [];
+
+  const Tile = styled('div')`
+  width: ${props => props.width || "auto"};
+  height: ${props => props.height || "auto"};
+  transform-origin: ${props => props.transformorigin || "0 0 0"};
+  transform: ${props => props.transform || "none"};
+  background-color: ${props => props.bgcolor || "none"};
+  `
+
+  let t = 1;
+  for (t = fuzzy; t < Math.PI; t += inc) {
+    let ka = t;
+    let z = radius * Math.cos(t);
+    let currentRadius = Math.abs((radius * Math.cos(0) * Math.sin(t)) - (radius * Math.cos(Math.PI) * Math.sin(t))) / 2.5;
+    let circumference = Math.abs(2 * Math.PI * currentRadius);
+    let squaresThatFit = Math.floor(circumference / squareSize);
+    let angleInc = (Math.PI * 2 - fuzzy) / squaresThatFit;
+
+    //let discoTiles;
+    let kb = 1;
+    for (var i = angleInc / 2 + fuzzy; i < (Math.PI * 2); i += angleInc) {
+
+      //let squareTileStyles = "";
+
+      //let squareStyles = "width: " + squareSize + "px; " 
+      //+ "height: " + squareSize + "px; " 
+      //+ "transform-origin: 0 0 0; " 
+      //+ "transform: rotate(" + i + "rad) rotateY(" + t + "rad);";
+
+      //squareTile.style.width = squareSize + "px";
+      //squareTile.style.height = squareSize + "px";
+      //squareTile.style.transformOrigin = "0 0 0";
+      //squareTile.style.transform = "rotate(" + i + "rad) rotateY(" + t + "rad)";
+
+      //squareTileStyles = "width:" + squareSize + "px;";
+      //squareTileStyles += "width:" +  squareSize + "px;";
+      //squareTileStyles += "transform:rotate(" + i + "rad) rotateY(" + t + "rad);";
+      //squareTileStyles += "transform-origin:0 0 0;";
+
+
+
+
+ 
+      let tilebg;
+      if((t>1.3 && t<1.9) || (t<-1.3 && t>-1.9)) {
+        tilebg = randomColor("bright");
+      }
+      else {
+        tilebg = randomColor("any");
+      }
+      
+     
+
+      let square = <Tile className="square"
+        key={ka+'-'+kb}
+        width={squareSize + 'px'}
+        height={squareSize + 'px'} 
+        transform-origin="0 0 0"
+        transform={"rotate(" + i + "rad) rotateY(" + t + "rad)"}
+        >
+
+        <Tile className="squareTile"
+                  key={'2'+ka+'-'+kb}
+                  width={squareSize + 'px'}
+                  height={squareSize + 'px'} 
+                  transform-origin="0 0 0"
+                  transform={"rotate(" + i + "rad) rotateY(" + t + "rad)"}
+                  bgcolor={tilebg}>
+        </Tile>
+      </Tile>;
+
+
+      //squareTile.style.animation = "reflect 2s linear infinite";
+      //squareTile.style.animationDelay = String(randomNumber(0,20)/10) + "s";
+      //squareTile.style.backfaceVisibility = "hidden";
+
+      let x = radius * Math.cos(i) * Math.sin(t);
+      let y = radius * Math.sin(i) * Math.sin(t);
+
+      //square.style.webkitTransform = "translateX(" + Math.ceil(x) + "px) translateY(" + y + "px) translateZ(" + z + "px)";
+      //square.style.transform = "translateX(" + x + "px) translateY(" + y + "px) translateZ(" + z + "px)";
+
+      //discoBall.appendChild(square);
+      discoTiles.push(square);
+      //console.log(discoTiles);
+      kb++;
+    }
+    //discoBall = <>{discoTiles}</>
+
+    //console.log(discoBall);
+
+  }
+
+  function randomColor(type) {
+    let r;
+    if (type == "bright") { r = randomNumber(130, 255); }
+    else { r = randomNumber(110, 190); }
+
+    return "rgb(" + r + "," + r + "," + r + ")";
+  }
+
+  function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
   return (
     <StyledDiscoBall>
-      <div className="discoball">
-        {grid}
-      </div>
+
+      <DiscoBallLight></DiscoBallLight>
+      <DiscoBall>
+
+        <DiscoBallMiddle>
+
+        </DiscoBallMiddle>
+
+        <>
+        {discoTiles}
+        </>
+
+      </DiscoBall>
+
     </StyledDiscoBall>
   )
 };
 
-export default DataMap;
+export default JungleDiscoBall;
+
+const background = (top, bottom) => css`
+  background: linear-gradient( to bottom,${top} 0%,${bottom} 100% );  
+`
 
 const StyledDiscoBall = styled.div`
- 
+  height: 300px;
+  position: relative;
+
+  .square {
+    transform-style: preserve-3d;	
+    position: absolute;
+    top: 50px;
+    left: 50px;
+    width: 6px;
+    height: 6px;
+    position: absolute;
+    transform: rotateX(90deg) rotateY(0deg) translateZ(0px);
+  }
+`;
+
+const DiscoBallLight = styled.div`
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  top: 50px;
+  left: 50%;
+  margin-left: -50px; 
+  border-radius: 100%;
+  background-color: white; 
+  opacity: 0.2;
+  -webkit-filter: blur(20px);
+`;
+
+const DiscoBallMiddle = styled.div`
+  height: 100%;
+  width: 100%;
+  border-radius: 100%;
+  background-color: #111;
+  position: absolute;
+  ${background("#333", "#111")};
+  animation: ${rotateDiscoBallMiddle} 18s linear infinite;
+`;
+
+const DiscoBall = styled.div`
+  transform-style: preserve-3d;
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  top: 50px;
+  left: 50%;
+  margin-left: -50px;
+  animation: ${rotateDiscoBall} 18s linear infinite;
 `;
